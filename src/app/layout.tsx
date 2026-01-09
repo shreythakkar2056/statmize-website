38ab1712d2b32854e6dcceb92feb5082e184b91a
Shrey Thakkar <shreythakkar2056@gmail.com>
2026-01-09 19:55:04 +0530

    anlytics and speed in vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 // ... imports ...
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "Statmize | Smart Wearable for Athletes", // <--- CHANGE THIS TEXT
   description: "Track speed, angle, and power with the Statmize smart band.",
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+         {/* Vercel Analytics */}
+        <Analytics/>
+        {/* Vercel Speed Insights */}
+        <SpeedInsights/>
+      </body>
       {/* ... rest of your layout code ... */}
     </html>
   );
